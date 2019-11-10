d5fd692c86dcbdb310d862cd5edd31e78b710d6a
Angel Rodriguez <arodriguez2552@gmail.com>
2019-11-10 16:39:03 -0500

    typeorm with postgresql config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
 import { HealthCheckService } from './app.health-check.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 
 @Module({
   imports: [TerminusModule.forRootAsync({
     useClass: HealthCheckService,
-  })],
+  }),
+  TypeOrmModule.forRoot()],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly connection: Connection) {}
+}
